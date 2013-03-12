997ca7b29f65c53f6feaf81467b25191f4680bbc
Steven Frank <stevenf@Lovehound.local>
2013-03-11 22:31:03 -0700

    First commit

---
+++ Source/content_script.js
@@ -0,0 +1,35 @@
+walk(document.body);
+
+function walk(node) 
+{
+	// I stole this function from here:
+	// http://is.gd/mwZp7E
+	
+	var child, next;
+
+	switch ( node.nodeType )  
+	{
+		case 1:  // Element
+		case 9:  // Document
+		case 11: // Document fragment
+			child = node.firstChild;
+			while ( child ) 
+			{
+				next = child.nextSibling;
+				walk(child);
+				child = next;
+			}
+			break;
+
+		case 3: // Text node
+			handleText(node);
+			break;
+	}
+}
+
+function handleText(textNode) 
+{
+	textNode.nodeValue = textNode.nodeValue.replace(/\bthe cloud\b/gi, "my butt");
+}
+
+
