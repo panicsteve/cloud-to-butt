ce727bfd94d42aa28d89569872f7bf4a42af53fd
Steven Frank <stevenf@panic.com>
2013-03-12 10:49:33 -0700

    improved case preservation

--- Source/content_script.js
+++ Source/content_script.js
@@ -29,7 +29,14 @@ function walk(node)
 
 function handleText(textNode) 
 {
-	textNode.nodeValue = textNode.nodeValue.replace(/\bThe cloud\b/g, "My butt").replace(/\bthe cloud\b/gi, "my butt");
+	var v = textNode.nodeValue;
+
+    v = v.replace(/\bThe Cloud\b/g, "My Butt");
+	v = v.replace(/\bThe cloud\b/g, "My butt");
+    v = v.replace(/\bthe Cloud\b/g, "my Butt");
+	v = v.replace(/\bthe cloud\b/g, "my butt");
+	
+	textNode.nodeValue = v;
 }
 
 
