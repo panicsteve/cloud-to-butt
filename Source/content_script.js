813bdeb25f5df749fa5f411a26d9853026e6642b
Erik <ralree@gmail.com>
2013-03-14 14:55:00 -0400

    Even more awesome

--- Source/content_script.js
+++ Source/content_script.js
@@ -31,14 +31,25 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
+  // Deal with the easy case
   v = v.replace(/\b(T|t)he (C|c)loud\b/g, function(match, p1, p2, offset, string) {
     // t - 7 = m
     // c - 1 = b
-    first = String.fromCharCode(p1.charCodeAt(0) - 7);
-    second = String.fromCharCode(p2.charCodeAt(0) - 1);
-    return first + "y " + second + "utt";
+    m = String.fromCharCode(p1.charCodeAt(0) - 7);
+    b = String.fromCharCode(p2.charCodeAt(0) - 1);
+    return m + "y " + b + "utt";
   });
-	
+  // Get the corner cases
+  if(v.match(/cloud/i)) {
+    // If we're not talking about weather
+    if(v.match(/PaaS|SaaS/IaaS/computing/data/storage/cluster/distributed/i) {
+      v = v.replace(/(C|c)loud/i, function(match, p1, p2, offset, string) {
+        // c - 1 = b
+        b = String.fromCharCode(p1.charCodeAt(0) - 7);
+        return b + "utt";
+      });
+    }
+  }
 	textNode.nodeValue = v;
 }
 
