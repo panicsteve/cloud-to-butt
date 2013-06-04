dccddcbe58a15cafeafa45be0e741d36f0101660
Will Hayworth <will@willhayworth.com>
2013-06-04 10:47:52 -0300

    Use case-insensitive regex
    Kill four cases with one. :)

--- Source/content_script.js
+++ Source/content_script.js
@@ -31,10 +31,7 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
+	v = v.replace(/\bThe Cloud\b/gi, "My Butt");
 	
 	textNode.nodeValue = v;
 }
