10c4086b43c6c801da70d24d7d66524758b5acda
Alex Hong <aqhong@gmail.com>
2014-10-13 13:24:06 -0700

    changed text replacement to sjw -> skeleton

--- Source/content_script.js
+++ Source/content_script.js
@@ -31,10 +31,10 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
+	v = v.replace(/\bSJW\b/g, "skeleton");
+	v = v.replace(/\bsjw\b/g, "skeleton");
+	v = v.replace(/\bSJWs\b/g, "skeletons");
+	v = v.replace(/\bsjws\b/g, "skeletons");
 	
 	textNode.nodeValue = v;
 }
