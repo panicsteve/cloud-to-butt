f94ac71c8e5921b666e1fc6bb160d0da7da1de53
Alex Hong <alex@alexhong.net>
2014-10-14 15:34:24 -0700

    integrated @mwhite's post-load fix

--- Source/content_script.js
+++ Source/content_script.js
@@ -1,5 +1,24 @@
 walk(document.body);
 
+if (window.MutationObserver) {
+	var observer = new MutationObserver(function (mutations) {
+		Array.prototype.forEach.call(mutations, function (m) {
+			if (m.type === 'childList') {
+				walk(m.target);
+			} else if (m.target.nodeType === 3) {
+				handleText(m.target);
+			}
+		});
+	});
+
+	observer.observe(document.body, {
+		childList: true,
+		attributes: false,
+		characterData: true,
+		subtree: true
+	});
+}
+
 function walk(node) 
 {
 	// I stole this function from here:
@@ -31,14 +50,17 @@ function walk(node)
 
 function handleText(textNode) 
 {
-	var v = textNode.nodeValue;
+	var oldValue = textNode.nodeValue;
+	var v = oldValue;
 
 	v = v.replace(/\bsjw\b/ig, "skeleton");
 	v = v.replace(/\bsjws\b/ig, "skeletons");
 	v = v.replace(/\bAn skeleton(s?)\b/g, "A skeleton$1");
 	v = v.replace(/\ban skeleton(s?)\b/g, "a skeleton$1");
 	
-	textNode.nodeValue = v;
+	if (v !== oldValue) {
+		textNode.nodeValue = v;
+	}
 }
 
 
