6c4bc48280e937f2fb71dbf6f876123955de8add
Chris Francis <chrisf@apple.com>
2015-02-20 17:18:06 +0000

    Robustified and awesomized the handling of different cases, as well as making 'the -> my' optional

--- Source/content_script.js
+++ Source/content_script.js
@@ -1,42 +1,48 @@
 walk(document.body);
 
-function walk(node) 
-{
-	// I stole this function from here:
-	// http://is.gd/mwZp7E
-	
-	var child, next;
+function walk(node) {
+  // I stole this function from here:
+  // http://is.gd/mwZp7E
+  
+  var child, next;
 
-	switch ( node.nodeType )  
-	{
-		case 1:  // Element
-		case 9:  // Document
-		case 11: // Document fragment
-			child = node.firstChild;
-			while ( child ) 
-			{
-				next = child.nextSibling;
-				walk(child);
-				child = next;
-			}
-			break;
+  switch ( node.nodeType ) {
+    case 1:  // Element
+    case 9:  // Document
+    case 11: // Document fragment
+      child = node.firstChild;
+      while ( child ) {
+        next = child.nextSibling;
+        walk(child);
+        child = next;
+      }
+      break;
 
-		case 3: // Text node
-			handleText(node);
-			break;
-	}
+    case 3: // Text node
+      handleText(node);
+      break;
+  }
 }
 
-function handleText(textNode) 
-{
-	var v = textNode.nodeValue;
+function handleText(textNode) {
+  var v = textNode.nodeValue
+    , rgx = /(?:([Tt])[Hh]([Ee])\s)?([Cc])[Ll]([Oo])[Uu]([Dd])/gm
+  ;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
-	
-	textNode.nodeValue = v;
+  v = v.replace(rgx, function(match, $1, $2, $3, $4, $5, $6) {
+    return (!!$1 ?
+      (
+        ($1 === 'T' ? 'M' : 'm') +
+        ($2 === 'E' ? 'Y' : 'y') + 
+        ' '
+      ) : ''
+    ) +
+    (
+      ($3 === 'C' ? 'B' : 'b') +
+      ($4 === 'O' ? 'U' : 'u') +
+      ($5 === 'D' ? 'TT' : 'tt')
+    );
+  });
+  
+  textNode.nodeValue = v;
 }
-
-
